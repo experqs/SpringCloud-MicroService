9ccc5235ec7326bae67ac2e5a66b02c0efb7f0f9
experqs <experqs@126.com>
2018-07-10 10:59:13 +0800

    master引入Lombok工具

--- common/src/main/java/com/dzc/common/model/Result.java
+++ common/src/main/java/com/dzc/common/model/Result.java
@@ -1,12 +1,18 @@
 package com.dzc.common.model;
 
 import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 /**
  * RESTful API 通用返回数据体
  */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Result<T> implements Serializable {
 
     @ApiModelProperty(value = "返回状态编码")
@@ -21,51 +27,10 @@ public class Result<T> implements Serializable {
     @ApiModelProperty(value = "返回附加信息")
     private Object extra;
 
-    public Result() {
-    }
-
     public Result(Integer code, String msg, T data) {
         this.code = code;
         this.msg = msg;
         this.data = data;
     }
 
-    public Result(Integer code, String msg, T data, Object extra) {
-        this.code = code;
-        this.msg = msg;
-        this.data = data;
-        this.extra = extra;
-    }
-
-    public Integer getCode() {
-        return code;
-    }
-
-    public void setCode(Integer code) {
-        this.code = code;
-    }
-
-    public String getMsg() {
-        return msg;
-    }
-
-    public void setMsg(String msg) {
-        this.msg = msg;
-    }
-
-    public T getData() {
-        return data;
-    }
-
-    public void setData(T data) {
-        this.data = data;
-    }
-
-    public Object getExtra() {
-        return extra;
-    }
-
-    public void setExtra(Object extra) {
-        this.extra = extra;
-    }
 }
